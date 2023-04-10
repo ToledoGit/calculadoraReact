08c5a8e4447892170da81a232f090bc0f437df98
ToledoGit <127156204+ToledoGit@users.noreply.github.com>
2023-04-10 12:31:03 -0300

    Desafio DIO

---
+++ src/components/Button/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #AAAAAA;
+    font-size:24px;
+    font-weight: 700;
+    flex: 1;
+    background-color:#00AAF0;
+    color:#FFFFFF;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width:100%;
+    height 75px;
+    background-color: #AAFFEA;
+
+    display:flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:24px;
+
+    input {
+        width: 100%;
+        height 75px;
+        background-color: #AAFFEA;
+        border: 0;
+        font-size:24px;
+        align: right;
+        padding: 0 10px;
+    }
+`
